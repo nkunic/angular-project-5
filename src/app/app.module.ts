466b0599b44b9e4450b73098692cac00af173de6
nkunic <natasa.kunic@gmail.com>
2022-03-17 21:50:27 +0100

    Working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-
 import { HomepageComponent } from './view/homepage/homepage.component';
 
 import { KnjigeComponent } from './view/knjige/knjige.component';
 import { KnjigaComponent } from './view/knjige/knjiga/knjiga.component';
-
 import { ClanoviComponent } from './view/clanovi/clanovi.component';
 import { ClanComponent } from './view/clanovi/clan/clan.component';
 
